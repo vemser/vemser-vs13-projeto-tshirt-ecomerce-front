d235904934268e76c0ba8d9cfe3f7e0183547d04
Lorenzo <lorenzohpanato@gmail.com>
2024-03-13 17:10:56 -0300

    feat: add home page structure

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,19 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Checkout from "../pages/Checkout/Checkout";
+import Home from "../pages/Home/Home";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <h1>Home</h1>
-            </>
-          }
-        />
+        <Route path="/" element={<Home />} />
 
         <Route path="/checkout" element={<Checkout />} />
       </Routes>

7f6b9a75564efe27548a145a69e38e9d0e8f10d7
CamilaVerso <camilaverso2@gmail.com>
2024-03-14 14:07:36 -0300

    fix:responsiveness

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,8 +1,8 @@
-import { yupResolver } from '@hookform/resolvers/yup';
-import { Button, TextField, Typography } from '@mui/material';
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Button, TextField, Typography } from "@mui/material";
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
-import backgroundImage from '../../../public/login.jpg'; 
+import backgroundImage from "../../../public/login.jpg";
 
 interface FormInputs {
   email: string;
@@ -15,41 +15,73 @@ const schema = yup.object().shape({
 });
 
 export const Login = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm<FormInputs>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormInputs>({
     resolver: yupResolver(schema),
   });
 
   const onSubmit = (data: FormInputs) => {
     console.log(data);
-    
   };
 
   return (
-    <main className="h-screen flex">
-      
-      <div className="w-1/2 h-full bg-cover bg-center" style={{ backgroundImage: `url(${backgroundImage})` }} />
-      
-      
-      <section className='w-1/2 flex items-center justify-center p-10'>
+    <main className="h-screen flex mx-auto justify-center">
+      <div
+        className="hidden md:block  h-full bg-cover bg-center sm:max-w-xl w-full"
+        style={{ backgroundImage: `url(${backgroundImage})` }}
+      />
+
+      {/* <section className="w-1/2 flex items-center justify-center p-10 max-w-xl"> */}
+      <section className="w-full md:w-1/2 flex items-center justify-center p-10 max-w-xl">
+
         <form onSubmit={handleSubmit(onSubmit)} className="w-full max-w-md">
-          <Typography variant="h4" className='mb-8 text-center'>
-              Bem-vindo 👋
-          </Typography>
-          <Typography variant="h5" className='mb-8 text-center'>
-              Por favor, insira suas credenciais
-          </Typography>
-          
-          <TextField
+
+          <h1 className="font-extrabold text-3xl ">Bem vindo</h1>
+          <p className="text-base mb-8">Por favor, insira suas credenciais</p>
+
+          <div className="mb-4">
+            <label htmlFor="email" className="block">
+              Endereço de e-mail
+            </label>
+            <input
+            {...register("email")}
+              type="email"
+              name="email"
+              placeholder="exemplo@email.com"
+              className="w-full px-3 py-2 border rounded-md"
+            />
+            {errors.email && <p className="text-red-500">{errors.email.message}</p>}
+          </div>
+
+          <div className="mb-4">
+            <label htmlFor="password" className="block">
+              Senha
+            </label>
+            <input
+            {...register("password")}
+              type="password"
+              name="password"
+              placeholder="Digite sua senha"
+              className="w-full px-3 py-2 border rounded-md"
+            />
+            {errors.password && <p className="text-red-500">{errors.password.message}</p>}
+          </div>
+
+          {/* <TextField
             {...register("email")}
             error={!!errors.email}
             helperText={errors.email?.message}
             label="Email"
+            InputLabelProps={{ shrink: true }}
             variant="outlined"
             fullWidth
-            className="mb-4"
-          />
-          
-          <TextField
+            sx={{ marginBottom: "1rem" }}
+          /> */}
+
+          {/* <TextField
             {...register("password")}
             error={!!errors.password}
             helperText={errors.password?.message}
@@ -58,16 +90,21 @@ export const Login = () => {
             variant="outlined"
             fullWidth
             className="mb-4"
-          />
-          <Typography variant="h6" className='mb-8 text-right'>
-              Esqueceu a senha?
-          </Typography>
-          
+          /> */}
+          {/* <Typography variant="h6" className="mb-8 text-right">
+            Esqueceu a senha?
+          </Typography> */}
+          <p className="mb-8 text-right cursor-pointer">Esqueceu a senha?</p>
+
           <Button
             type="submit"
             variant="contained"
             fullWidth
-            sx={{background: "#121212", '&:hover': { backgroundColor: '#0e0e0e' }, mt:2}}
+            sx={{
+              background: "#121212",
+              "&:hover": { backgroundColor: "#0e0e0e" },
+              mt: 2,
+            }}
           >
             Login
           </Button>

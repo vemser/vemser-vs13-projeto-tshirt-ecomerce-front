8bee674b08579505a0a4bdd8f150645c5a5c3864
Welthanos <welthanos_dev@outlook.com>
2024-03-14 17:42:59 -0300

    style: hero section

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,18 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DefaultLayout from "../layout/DefaultLayout";
 import Checkout from "../pages/Checkout/Checkout";
-import Header from "../components/Header/Header";
-import { Login } from "../pages/login/Login";
+import { NotFound } from "../pages/NotFound/NotFound";
 import { Register } from "../pages/Register/Register";
+import { Login } from "../pages/login/Login";
 import Home from "../pages/Home/Home";
 
 export default function Router() {
   return (
     <BrowserRouter>
-      <Header />
-
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/checkout" element={<Checkout />} />
+        <Route element={<DefaultLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Route>
+        
+        <Route path="*" element={<NotFound />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>

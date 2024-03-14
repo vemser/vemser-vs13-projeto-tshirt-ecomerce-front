0dd90f59268646df43a662e5d5163f6f69e04dc3
CamilaVerso <camilaverso2@gmail.com>
2024-03-14 16:05:17 -0300

    style: finished responsiveness

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,8 +1,8 @@
 import { yupResolver } from "@hookform/resolvers/yup";
-import { Button, TextField, Typography } from "@mui/material";
+import { Button } from "@mui/material";
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
-import backgroundImage from "../../../public/login.jpg";
+import womanImage from "../../../src/assets/foto-mulher-jovem.svg";
 
 interface FormInputs {
   email: string;
@@ -28,32 +28,38 @@ export const Login = () => {
   };
 
   return (
-    <main className="h-screen flex mx-auto justify-center">
-      <div
-        className="hidden md:block  h-full bg-cover bg-center sm:max-w-xl w-full"
-        style={{ backgroundImage: `url(${backgroundImage})` }}
-      />
+    <main className="flex md:flex-row flex-col">
+      <figure className="md:w-2/4 w-full md:h-screen h-5/6 bg-tertiary flex justify-center items-end">
+        <img
+          className="w-4/6 h-4/6"
+          src={womanImage}
+          alt={"Imagem mulher ruiva jovem"}
+        />
+      </figure>
 
-      {/* <section className="w-1/2 flex items-center justify-center p-10 max-w-xl"> */}
-      <section className="w-full md:w-1/2 flex items-center justify-center p-10 max-w-xl">
-
+      <section className="w-full md:w-2/4 flex flex-col h-screen p-4 justify-center items-center">
         <form onSubmit={handleSubmit(onSubmit)} className="w-full max-w-md">
-
-          <h1 className="font-extrabold text-3xl ">Bem vindo</h1>
-          <p className="text-base mb-8">Por favor, insira suas credenciais</p>
+          <div className="flex flex-col gap-1">
+            <h1 className="text-3xl font-bold">Bem vindo</h1>
+            <p className="text-base text-gray-400 mb-7">
+              Por favor, insira suas credenciais
+            </p>
+          </div>
 
           <div className="mb-4">
             <label htmlFor="email" className="block">
               Endereço de e-mail
             </label>
             <input
-            {...register("email")}
+              {...register("email")}
               type="email"
               name="email"
               placeholder="exemplo@email.com"
-              className="w-full px-3 py-2 border rounded-md"
+              className="w-full px-3 py-2 border-black border rounded-md"
             />
-            {errors.email && <p className="text-red-500">{errors.email.message}</p>}
+            {errors.email && (
+              <p className="text-red-500">{errors.email.message}</p>
+            )}
           </div>
 
           <div className="mb-4">
@@ -61,39 +67,17 @@ export const Login = () => {
               Senha
             </label>
             <input
-            {...register("password")}
+              {...register("password")}
               type="password"
               name="password"
               placeholder="Digite sua senha"
-              className="w-full px-3 py-2 border rounded-md"
+              className="w-full px-3 py-2 border-black border rounded-md"
             />
-            {errors.password && <p className="text-red-500">{errors.password.message}</p>}
+            {errors.password && (
+              <p className="text-red-500">{errors.password.message}</p>
+            )}
           </div>
 
-          {/* <TextField
-            {...register("email")}
-            error={!!errors.email}
-            helperText={errors.email?.message}
-            label="Email"
-            InputLabelProps={{ shrink: true }}
-            variant="outlined"
-            fullWidth
-            sx={{ marginBottom: "1rem" }}
-          /> */}
-
-          {/* <TextField
-            {...register("password")}
-            error={!!errors.password}
-            helperText={errors.password?.message}
-            label="Senha"
-            type="password"
-            variant="outlined"
-            fullWidth
-            className="mb-4"
-          /> */}
-          {/* <Typography variant="h6" className="mb-8 text-right">
-            Esqueceu a senha?
-          </Typography> */}
           <p className="mb-8 text-right cursor-pointer">Esqueceu a senha?</p>
 
           <Button
@@ -103,7 +87,6 @@ export const Login = () => {
             sx={{
               background: "#121212",
               "&:hover": { backgroundColor: "#0e0e0e" },
-              mt: 2,
             }}
           >
             Login

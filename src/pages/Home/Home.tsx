e0a683e79bfa2c238064f079b34cc45af6cf18bd
Welthanos <welthanos_dev@outlook.com>
2024-03-15 14:27:15 -0300

    style: hero section image

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,29 +6,17 @@ import casualCategory from "../../assets/category-casual.png";
 const Home = () => {
   return (
     <>
-      <section
-        id="hero"
-        className="w-full h-screen flex items-center justify-center pt-10 pb-40"
-      >
+      <section id="hero" className="w-full h-[90vh] flex items-center justify-center pt-10 pb-40">
         <div className="w-full max-w-8xl h-full flex items-center justify-around bg-secondary bg-opacity-60 gap-6 px-10 rounded-lg border border-tertiary border-opacity-10">
           <div className="">
             <h1 className="text-6xl font-serif font-bold mb-5">
               Descubra o seu estilo <br /> na iShirts
             </h1>
             <p className="text-3xl font-sans mb-10">Descontos de até 40% OFF</p>
-            <a
-              href=""
-              className="font-sans bg-primary text-white px-5 py-3 rounded-lg"
-            >
-              COMPRAR AGORA
-            </a>
+            <a href="" className="font-sans bg-primary text-white px-5 py-3 rounded-lg">COMPRAR AGORA</a>
           </div>
 
-          <img
-            src={hero}
-            className="self-end h-4/5"
-            alt="Imagem de uma modelo posando com camiseta preta e chapéu branco"
-          />
+          <img src={hero} className="self-end h-5/6" alt="Imagem de uma modelo posando com camiseta preta e chapéu branco" />
         </div>
       </section>
 

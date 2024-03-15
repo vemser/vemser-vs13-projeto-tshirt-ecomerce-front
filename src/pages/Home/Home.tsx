bf11ea13fc2b0754f65cf98bf977ed48f99c4a2f
Welthanos <welthanos_dev@outlook.com>
2024-03-15 17:09:48 -0300

    feat: feedback section

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,7 +6,13 @@ import tShirt1 from "../../assets/t-shirt-1.png";
 import tShirt2 from "../../assets/t-shirt-2.png";
 import tShirt3 from "../../assets/t-shirt-3.png";
 import tShirt4 from "../../assets/t-shirt-4.png";
+import profile1 from "../../assets/profile1.svg";
+import profile2 from "../../assets/profile2.svg";
+import profile3 from "../../assets/profile3.svg";
+import profile4 from "../../assets/profile4.svg";
+import quotes from "../../assets/quotes.svg";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { LuChevronRight, LuChevronLeft } from 'react-icons/lu';
 import "swiper/css";
 
 const tShirtsList = [
@@ -54,7 +60,39 @@ const tShirtsList = [
   },
 ];
 
+const feedbacks = [
+  {
+    name: 'Georgia Murazik',
+    text: 'Ab amet reprehenderit saepe quia veniam. Voluptas dolore officia sequi aut rerum molestiae iure. Laborum dicta qui praesentium laborum sint.',
+    img: profile2
+  },
+  {
+    name: 'Alison Nikolaus',
+    text: 'Ab amet reprehenderit saepe quia veniam. Voluptas dolore officia sequi aut rerum molestiae iure. Laborum dicta qui praesentium laborum sint.',
+    img: profile3
+  },
+  {
+    name: 'Camille Ferros',
+    text: 'Ab amet reprehenderit saepe quia veniam. Voluptas dolore officia sequi aut rerum molestiae iure. Laborum dicta qui praesentium laborum sint.',
+    img: profile1
+  },
+  {
+    name: 'Rafael Lazzari',
+    text: 'Ab amet reprehenderit saepe quia veniam. Voluptas dolore officia sequi aut rerum molestiae iure. Laborum dicta qui praesentium laborum sint.',
+    img: profile4
+  }
+]
+
 const Home = () => {
+  const handleScrollLeft = () => {
+    const div = document.getElementById('swiper');
+    if (div) div.scrollLeft -= 200;
+  }
+  const handleScrollRight = () => {
+    const div = document.getElementById('swiper');
+    if (div) div.scrollLeft += 200;
+  }
+
   return (
     <>
       <section id="hero" className="w-full h-[90vh] flex items-center justify-center pt-10 pb-40">
@@ -66,7 +104,6 @@ const Home = () => {
             <p className="text-3xl font-sans mb-10">Descontos de até 40% OFF</p>
             <a href="" className="font-sans bg-primary text-white px-5 py-3 rounded-lg">COMPRAR AGORA</a>
           </div>
-
           <img src={hero} className="self-end h-5/6" alt="Imagem de uma modelo posando com camiseta preta e chapéu branco" />
         </div>
       </section>
@@ -118,13 +155,13 @@ const Home = () => {
           </button>
         </div>
         <div className="mt-10 text-lg">
-          <Swiper spaceBetween={20} slidesPerView={4} className="overflow-x-hidden flex">
+          <Swiper spaceBetween={20} slidesPerView={4.2} className="overflow-x-hidden flex">
             {tShirtsList.map((tShirt, index) => (
               <SwiperSlide key={index}>
                 <div className="flex flex-col gap-3 max-w-80 cursor-pointer">
                   <img
                     src={tShirt.img}
-                    alt="..."
+                    alt="Modelo de camiseta iShirts"
                     className="border border-tertiary border-opacity-30 rounded-xl"
                   />
                   <div>
@@ -142,6 +179,32 @@ const Home = () => {
                         R${tShirt.price.toFixed(2)}
                       </p>{" "}
                     </div>
+                  </div>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      </section>
+
+      <section id="feedbacks" className="px-4 mt-24">
+        <div className="flex justify-between items-center gap-2">
+          <h2 className="font-bold text-2xl md:text-4xl sm:text-3xl">O que nossos clientes dizem</h2>
+          <div className="flex gap-4">
+            <button onClick={handleScrollLeft} className="bg-tertiary text-xl text-center text-secondary font-extrabold p-2 pe-2.5 rounded-lg transition hover:opacity-90"><LuChevronLeft /></button>
+            <button onClick={handleScrollRight} className="bg-primary text-xl text-center text-secondary font-extrabold p-2 pe-2.5 rounded-lg transition hover:opacity-90"><LuChevronRight /></button>
+          </div>
+        </div>
+        <div className="mt-10 text-lg">
+          <Swiper id="swiper" spaceBetween={30} slidesPerView={2.5} className="overflow-x-hidden flex mb-20" style={{ scrollBehavior: 'smooth', transition: 'scroll 0.8s' }}>
+            {feedbacks.map((feedback, index) => (
+              <SwiperSlide key={index}>
+                <div className="flex flex-col gap-7 p-8 bg-secondary bg-opacity-45 border border-tertiary border-opacity-15 rounded-xl">
+                  <img className="w-10" src={quotes} alt="" />
+                  <p className="font-medium">{feedback.text}</p>
+                  <div className="flex items-center gap-3">
+                    <img className="h-10 w-10 rounded-full" src={feedback.img} alt={`Imagem de usuário de ${feedback.name}`} />
+                    <strong>{feedback.name}</strong>
                   </div>
                 </div>
               </SwiperSlide>

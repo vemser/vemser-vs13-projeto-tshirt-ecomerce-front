8d7940dfa4cc741b1f020c6dc363d08065a6d735
unknown <danielangelo1234@gmail.com>
2024-03-21 15:18:09 -0300

    fix: add props and map to bankcard

--- src/pages/Profile/PaymentsMethod/PaymentsMethod.tsx
+++ src/pages/Profile/PaymentsMethod/PaymentsMethod.tsx
@@ -1,6 +1,21 @@
 import { LuPlus } from "react-icons/lu";
 import BankCard from "../../../components/Profile/BankCard/BankCard";
 
+const cards = [
+  {
+    cardCompany: "Mastercard",
+    cardNumber: "**** **** **** 1234",
+  },
+  {
+    cardCompany: "Visa",
+    cardNumber: "**** **** **** 5678",
+  },
+  {
+    cardCompany: "Elo",
+    cardNumber: "**** **** **** 9012",
+  },
+];
+
 const PaymentsMethod = () => {
   const handleEditProfile = () => {
     console.log("Edit Profile");
@@ -14,8 +29,14 @@ const PaymentsMethod = () => {
       >
         <LuPlus /> Adicionar Cartão
       </button>
-      <div className="flex justify-between items-center ">
-        <BankCard />
+      <div>
+        {cards.map((card, index) => (
+          <BankCard
+            key={index}
+            cardCompany={card.cardCompany}
+            cardNumber={card.cardNumber}
+          />
+        ))}
       </div>
     </>
   );
